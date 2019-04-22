d7e4f54c92768ac49f818b38176c46412bfcacc0
Adrian Kyzer <adriankyzer@gmail.com>
2019-04-22 14:57:56 +0200

    got data to display in material table

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { DataSource } from '@angular/cdk/collections';
 
+import { City } from '../../models/city.model';
 @Component({
-  selector: 'app-table',
-  templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+	selector: 'app-table',
+	templateUrl: './table.component.html',
+	styleUrls: [ './table.component.scss' ]
 })
 export class TableComponent implements OnInit {
+	@Input('cities') cities: City[];
 
-  constructor() { }
+	displayedColumns = [ 'city', 'content', 'latitude', 'longitude' ];
 
-  ngOnInit() {
-  }
+	constructor() {}
 
+	ngOnInit() {}
 }
--- src/app/material.ts
+++ src/app/material.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatMenuModule} from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
-	imports: [ MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatMenuModule ],
-	exports: [ MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatMenuModule ]
+	imports: [ MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatMenuModule, MatTableModule ],
+	exports: [ MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatMenuModule, MatTableModule ]
 })
 export class MaterialModule {}
